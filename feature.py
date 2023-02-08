4a5699b60970e901396d87af0cbb043a8ca4add9
TzuJuLin <b05102085@ntu.edu.tw>
2023-02-08 22:53:01 +0100

    update class structure

--- feature.py
+++ feature.py
@@ -15,71 +15,89 @@ class FeatureDict():
 
     def get_arc(self, sent):
         arc_set = []
-        for i in range(len(sent.id)):
-            if sent.rel[i] != "P" and sent.rel[i] != "ROOT":
-                arc_set.append((int(sent.head[i]), int(sent.id[i])))
+        for token in sent.tokens:
+            if token.rel != "P" and token.form != "ROOT":
+                arc_set.append((int(token.head), int(token.id)))
         return arc_set
 
-    def get_feature(self):
-        for i in range(len(self.corpus.sentences)):
-            sentence = self.corpus.sentences[i]
-            arc_set = self.get_arc(sentence)
+    def extract_feature(self):
+        for sent in self.corpus.sentences:
+
+            arc_set = self.get_arc(sent)
             for arc in arc_set:
                 '''
                 unigram features
                 every index has to be -1 since the corpus do not include ROOT
                 '''
                 # p-word
-                if sentence.form[arc[0]-1] not in self.feature_dict:
-                    self.feature_dict[sentence.form[arc[0]-1]] = 1
+                '''
+                要在dictionary的key上面加上是head 還是 child
+                
+                '''
+                if "p-w:" + sent.tokens[arc[0]].form not in self.feature_dict:
+                    self.feature_dict["p-w:" +
+                                      sent.tokens[arc[0]].form] = 0
+
                 # p-pos
-                if sentence.pos[arc[0]-1] not in self.feature_dict:
-                    self.feature_dict[sentence.pos[arc[0]-1]] = 1
+                if "p-p:" + sent.tokens[arc[0]].pos not in self.feature_dict:
+                    self.feature_dict["p-p:" + sent.tokens[arc[0]].pos] = 0
+
                 # (p-word,p-pos)
-                if (sentence.form[arc[0]-1], sentence.pos[arc[0]-1]) not in self.feature_dict:
-                    self.feature_dict[(sentence.form[arc[0]-1],
-                                       sentence.pos[arc[0]-1])] = 1
+                if "(p-w,p-p):" + str((
+                        sent.tokens[arc[0]].form, sent.tokens[arc[0]].pos)) not in self.feature_dict:
+                    self.feature_dict["(p-w,p-p):" + str((
+                        sent.tokens[arc[0]].form, sent.tokens[arc[0]].pos))] = 0
                 # c-word
-                if sentence.form[arc[1]-1] not in self.feature_dict:
-                    self.feature_dict[sentence.form[arc[0]-1]] = 1
+                if "c-w:" + sent.tokens[arc[1]].form not in self.feature_dict:
+                    self.feature_dict["c-w:" +
+                                      sent.tokens[arc[1]].form] = 0
+
                 # c-pos
-                if sentence.pos[arc[1]-1] not in self.feature_dict:
-                    self.feature_dict[sentence.pos[arc[0]-1]] = 1
+                if "c-p:" + sent.tokens[arc[1]].pos not in self.feature_dict:
+                    self.feature_dict["c-p:" + sent.tokens[arc[1]].pos] = 0
+
                 # (c-word, c-pos)
-                if (sentence.form[arc[1]-1], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[(sentence.form[arc[1]-1],
-                                       sentence.pos[arc[1]-1])] = 1
+                if "(c-w,c-p):" + str((
+                        sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos)) not in self.feature_dict:
+                    self.feature_dict["(c-w,c-p):" + str(
+                        (sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos))] = 0
                 '''
                 basic bigram features
                 '''
                 # (p-word, p-pos, c-word, c-pos)
-                if (sentence.form[arc[0]-1], sentence.pos[arc[0]-1], sentence.form[arc[1]-1], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[(sentence.form[arc[0]-1], sentence.pos[arc[0]-1],
-                                       sentence.form[arc[1]-1], sentence.pos[arc[1]-1])] = 1
+                if "(p-w,p-p,c-w,c-p):" + str((sent.tokens[arc[0]].form, sent.tokens[arc[0]].pos, sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos))not in self.feature_dict:
+                    self.feature_dict["(p-w,p-p,c-w,c-p):" + str((sent.tokens[arc[0]].form,
+                                                                  sent.tokens[arc[0]].pos, sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos))] = 0
+
                 # (p-pos, c-word, c-pos)
-                if (sentence.pos[arc[0]-1], sentence.form[arc[1]-1], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[sentence.pos[arc[0]-1],
-                                      sentence.form[arc[1]-1], sentence.pos[arc[1]-1]] = 1
+                if "(p-p,c-w,c-p):" + str((
+                        sent.tokens[arc[0]].pos, sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos)) not in self.feature_dict:
+                    self.feature_dict["(p-p,c-w,c-p):" + str((
+                        sent.tokens[arc[0]].pos, sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos))] = 0
                 # (p-word, c-word, c-pos)
-                if (sentence.form[arc[0]-1], sentence.form[arc[1]-1], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[sentence.form[arc[0]-1],
-                                      sentence.form[arc[1]-1], sentence.pos[arc[1]-1]] = 1
+                if "(p-w,c-w,c-p):" + str((
+                        sent.tokens[arc[0]].form, sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos)) not in self.feature_dict:
+                    self.feature_dict["(p-w,c-w,c-p):" + str((
+                        sent.tokens[arc[0]].form, sent.tokens[arc[1]].form, sent.tokens[arc[1]].pos))] = 0
+
                 # (p-word, p-pos, c-pos)
-                if (sentence.form[arc[0]-1], sentence.pos[arc[0]-1], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[(
-                        sentence.form[arc[0]-1], sentence.pos[arc[0]-1], sentence.pos[arc[1]-1])] = 1
+                if ("(p-w,p-p,c-p):" + str((sent.tokens[arc[0]].form, sent.tokens[arc[0]].pos, sent.tokens[arc[1]].pos))) not in self.feature_dict:
+                    self.feature_dict["(p-w,p-p,c-p):" + str(
+                        (sent.tokens[arc[0]].form, sent.tokens[arc[0]].pos, sent.tokens[arc[1]].pos))] = 0
                 # (p-word, p-pos, c-word)
-                if (sentence.form[arc[0]-1], sentence.pos[arc[0]-1], sentence.form[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[(
-                        sentence.form[arc[0]-1], sentence.pos[arc[0]-1], sentence.form[arc[1]-1])] = 1
+                if ("(p-w,p-p,c-w):" + str((sent.tokens[arc[0]].form, sent.tokens[arc[0]].pos, sent.tokens[arc[1]].form))) not in self.feature_dict:
+                    self.feature_dict["(p-w,p-p,c-w):" + str((sent.tokens[arc[0]].form,
+                                                              sent.tokens[arc[0]].pos, sent.tokens[arc[1]].form))] = 0
                 # (p-word, c-word)
-                if (sentence.form[arc[0]-1], sentence.form[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[(sentence.form[arc[0]-1],
-                                       sentence.form[arc[1]-1])] = 1
+                if ("(p-w,c-w):" + str((sent.tokens[arc[0]].form, sent.tokens[arc[1]].form))) not in self.feature_dict:
+                    self.feature_dict["(p-w,c-w):" + str(
+                        (sent.tokens[arc[0]].form, sent.tokens[arc[1]].form))] = 0
+
                 # (p-pos, c-pos)
-                if (sentence.pos[arc[0]-1], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                    self.feature_dict[(sentence.pos[arc[0]-1],
-                                       sentence.pos[arc[1]-1])] = 1
+                if "(p-p,c-p):" + str((
+                        sent.tokens[arc[0]].pos, sent.tokens[arc[1]].pos)) not in self.feature_dict:
+                    self.feature_dict["(p-p,c-p):" + str((
+                        sent.tokens[arc[0]].pos, sent.tokens[arc[1]].pos))] = 0
 
                 '''
                 In between POS features
@@ -89,43 +107,42 @@ class FeatureDict():
                 # check if  the parent and child are immediate neighbors
                 if abs(arc[1]-arc[0]) != 1:
                     # initialize a list that has the p-pos as first entry
-                    pos_in_between = [sentence.pos[arc[0]-1]]
                     # if child is at the right of parent
-                    if arc[1] > arc[0]:
-                        for i in range(arc[0], arc[1]-1):
-                            pos_in_between.append(sentence.pos[i])
+                    if arc[0] < arc[1]:
+                        for i in range(arc[0]+1, arc[1]):
+                            if ("b-w:" + sent.tokens[i].form) not in self.feature_dict:
+                                self.feature_dict["b-w:" +
+                                                  sent.tokens[i].form] = 0
                     # if child is at the left (bigger index than parent)
                     else:
-                        for i in range(arc[1], arc[0]-1):
-                            pos_in_between.append(sentence.pos[i])
-                    # append c-pos
-                    pos_in_between.append(sentence.pos[arc[1]-1])
-                    # get a tuple of the list
-                    pos_in_between_tuple = tuple(pos_in_between)
-                    if pos_in_between_tuple not in self.feature_dict:
-                        self.feature_dict[pos_in_between_tuple] = 1
+                        for i in range(arc[1]-1, arc[0]):
+                            if ("b-w:" + sent.tokens[i].form) not in self.feature_dict:
+                                self.feature_dict["b-w:" +
+                                                  sent.tokens[i].form] = 0
 
                     '''
                     surrounding word POS features
                     '''
                     # (p-pos, p-pos+1, c-pos-1, c-pos)
-                    if (sentence.pos[arc[0]-1], sentence.pos[arc[0]], sentence.pos[arc[1]-2], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                        self.feature_dict[(sentence.pos[arc[0]-1], sentence.pos[arc[0]],
-                                           sentence.pos[arc[1]-2], sentence.pos[arc[1]-1])] = 1
+                    if "p-p,p-p+1,c-p-1,c-p" + str((sent.tokens[arc[0]].pos, sent.tokens[arc[0]+1].pos, sent.tokens[arc[1]-1].pos, sent.tokens[arc[1]].pos)) not in self.feature_dict:
+                        self.feature_dict["p-p,p-p+1,c-p-1,c-p" + str(
+                            (sent.tokens[arc[0]].pos, sent.tokens[arc[0]+1].pos, sent.tokens[arc[1]-1].pos, sent.tokens[arc[1]].pos))] = 0
+
                     # (p-pos, p-pos-1, c-pos-1, c-pos)
-                    if (sentence.pos[arc[0]-2], sentence.pos[arc[0]-1], sentence.pos[arc[1]-2], sentence.pos[arc[1]-1]) not in self.feature_dict:
-                        self.feature_dict[(sentence.pos[arc[0]-2], sentence.pos[arc[0]-1],
-                                           sentence.pos[arc[1]-2], sentence.pos[arc[1]-1])] = 1
-                     # (p-pos, p-pos+1, c-pos, c-pos+1)
-                    if (sentence.pos[arc[0]-1], sentence.pos[arc[0]], sentence.pos[arc[1]-1], sentence.pos[arc[1]]) not in self.feature_dict:
-                        self.feature_dict[(sentence.pos[arc[0]-1], sentence.pos[arc[0]],
-                                           sentence.pos[arc[1]-1], sentence.pos[arc[1]])] = 1
+                    if "p-p,p-p-1,c-p-1,c-p" + str((sent.tokens[arc[0]].pos, sent.tokens[arc[0]-1].pos, sent.tokens[arc[1]-1].pos, sent.tokens[arc[1]].pos)) not in self.feature_dict:
+                        self.feature_dict["p-p,p-p-1,c-p-1,c-p" + str(
+                            (sent.tokens[arc[0]].pos, sent.tokens[arc[0]-1].pos, sent.tokens[arc[1]-1].pos, sent.tokens[arc[1]].pos))] = 0
+                    # (p-pos, p-pos+1, c-pos, c-pos+1)
+                    if "p-p,p-p+1,c-p,c-p+1" + str((sent.tokens[arc[0]].pos, sent.tokens[arc[0]+1].pos, sent.tokens[arc[1]].pos, sent.tokens[arc[1]+1].pos)) not in self.feature_dict:
+                        self.feature_dict["p-p,p-p+1,c-p,c-p+1" + str(
+                            (sent.tokens[arc[0]].pos, sent.tokens[arc[0]+1].pos, sent.tokens[arc[1]].pos, sent.tokens[arc[1]+1].pos))] = 0
+
                      # (p-pos-1, p-pos, c-pos, c-pos+1)
-                    if (sentence.pos[arc[0]-2], sentence.pos[arc[0]-1], sentence.pos[arc[1]-1], sentence.pos[arc[1]]) not in self.feature_dict:
-                        self.feature_dict[(sentence.pos[arc[0]-2], sentence.pos[arc[0]-1],
-                                           sentence.pos[arc[1]-1], sentence.pos[arc[1]])] = 1
+                    if "p-p-1,p-p,c-p,c-p+1" + str((sent.tokens[arc[0]-1].pos, sent.tokens[arc[0]].pos, sent.tokens[arc[1]].pos, sent.tokens[arc[1]+1].pos)) not in self.feature_dict:
+                        self.feature_dict["p-p,p-p+1,c-p,c-p+1" + str(
+                            (sent.tokens[arc[0]-1].pos, sent.tokens[arc[0]].pos, sent.tokens[arc[1]].pos, sent.tokens[arc[1]+1].pos))] = 0
         return self.feature_dict
 
 
 test = FeatureDict("/Users/lintzuru/Desktop/WS22:23/parsing/test_feature")
-print(len(test.get_feature()))
+print(test.extract_feature())
--- reader.py
+++ reader.py
@@ -5,16 +5,22 @@ def read(file):
         print(line)
 
 
+class Token:
+
+    def __init__(self):
+        self.id = None
+        self.form = None
+        self.lemma = None
+        self.pos = None
+        self.xpos = None
+        self.morph = None
+        self.head = None
+        self.rel = None
+
+
 class Sentence:
     def __init__(self):
-        self.id = []
-        self.form = []
-        self.lemma = []
-        self.pos = []
-        self.xpos = []
-        self.morph = []
-        self.head = []
-        self.rel = []
+        self.tokens = []
 
 
 class Corpus:
@@ -27,20 +33,32 @@ class Corpus:
         f = open(self.path, 'r')
         sent = Sentence()
         for line in f:
+            token = Token()
             if line != "\n":
                 current_line = line.split("\t")
-                # if current_line[0] == "1":
-                #     sent.id.append(0)
-                #     sent.form.append('ROOT')
-                #     sent.lemma.append('ROOT')
-                sent.id.append(current_line[0])
-                sent.form.append(current_line[1])
-                sent.lemma.append(current_line[2])
-                sent.pos.append(current_line[3])
-                sent.xpos.append(current_line[4])
-                sent.morph.append(current_line[5])
-                sent.head.append(current_line[6])
-                sent.rel.append(current_line[7])
+                if current_line[0] == "1":
+                    token.id = 0
+                    token.form = 'ROOT'
+                    token.pos = 'ROOT'
+                    sent.tokens.append(token)
+                    token = Token()
+                token.id = current_line[0]
+                token.form = current_line[1]
+                token.lemma = current_line[2]
+                token.pos = current_line[3]
+                token.xpos = current_line[4]
+                token.morph = current_line[5]
+                token.head = current_line[6]
+                token.rel = current_line[7]
+                sent.tokens.append(token)
+                # sent.append(current_line[0])
+                # sent.form.append(current_line[1])
+                # sent.lemma.append(current_line[2])
+                # sent.pos.append(current_line[3])
+                # sent.xpos.append(current_line[4])
+                # sent.morph.append(current_line[5])
+                # sent.head.append(current_line[6])
+                # sent.rel.append(current_line[7])
             else:
                 self.sentences.append(sent)
                 sent = Sentence()
@@ -48,23 +66,23 @@ class Corpus:
     def write_file(self):
         new_f = open("new.pred", "w")
         for sent in self.sentences:
-            for i in range(len(sent.id)):
+            for token in sent.tokens:
                 new_line = ""
-                new_line += sent.id[i]
+                new_line += token.id
                 new_line += "\t"
-                new_line += sent.form[i]
+                new_line += token.form
                 new_line += "\t"
-                new_line += sent.lemma[i]
+                new_line += token.lemma
                 new_line += "\t"
-                new_line += sent.pos[i]
+                new_line += token.pos
                 new_line += "\t"
-                new_line += sent.xpos[i]
+                new_line += token.xpos
                 new_line += "\t"
-                new_line += sent.morph[i]
+                new_line += token.morph
                 new_line += "\t"
-                new_line += sent.head[i]
+                new_line += token.head
                 new_line += "\t"
-                new_line += sent.rel[i]
+                new_line += token.rel
                 new_line += "\t"
                 new_line += "_"
                 new_line += "\t"
@@ -74,6 +92,8 @@ class Corpus:
             new_f.write("\n")
 
 
-# c = Corpus("/Users/lintzuru/Desktop/WS22:23/parsing/test_feature")
-# c.add_sentence()
-# print(type(c.sentences))
+c = Corpus("/Users/lintzuru/Desktop/WS22:23/parsing/test_feature")
+c.add_sentence()
+
+for token in c.sentences[0].tokens:
+    print(token.form)
